a360a1baca7a77a1e29a1608bc2934cfabdbfb19
changyuanliu <changyuan@ustc.edu>
2021-01-07 12:54:34 -0500

    change z_norm using while

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -62,7 +62,15 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   VectorXd z_pred = VectorXd(3);
   VectorXd z_norm = z;
   cout << "z = " << z << endl;
-  z_norm(1) = fmod(z_norm(1), 2*atan(1)*4);
+  while(z_norm(1)<-atan(1)*4)
+  {
+    z_norm(1) += 2*atan(1)*4;
+  }
+  while(z_norm(1)>atan(1)*4)
+  {
+    z_norm(1) -= 2*atan(1)*4;
+  }
+  // z_norm(1) = fmod(z_norm(1), 2*atan(1)*4);
   cout << "z_norm = " << z_norm << endl;
   // cout << "z = " << z << endl;
   z_pred << sqrt(x_[0]*x_[0]+x_[1]*x_[1]), 
